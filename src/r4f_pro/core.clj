7d2a9833e8102aee04f24b4026b7b7308d586da8
Ruslan Sorokin <ru@Ruslans-iMac.local>
2016-10-19 19:23:40 +0300

    cleared protege filechuser

--- src/r4f_pro/core.clj
+++ src/r4f_pro/core.clj
@@ -6,5 +6,5 @@
   (println "\nProtege-3.5")
   (println "A free, open-source ontology editor and framework for building intelligent systems")
   (println "(http://protege.stanford.edu/)\n")
-  (edu.stanford.smi.protege.Application/main (into-array ["pro-projects/Examples.pprj"])))
+  (edu.stanford.smi.protege.Application/main (make-array String 0)))
 
